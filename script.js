fa685989f53fd98288025c3f8965c0c43a3b1d90
Calvin Roberts <roberts.calvin85@gmail.com>
2023-09-11 23:11:50 +0100

    Create function to add raindrops

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 'use strict';
+function rain() {
+  let cloud = document.querySelector('.cloud'),
+    e = document.createElement('div');
+
+  e.classList.add('drop');
+  cloud.appendChild(e);
+
+  setTimeout(function () {
+    cloud.removeChild(e);
+  }, 2000);
+}
+
+setInterval(function () {
+  rain();
+}, 20);
