6237ce39adcdcbe8e3b8b62ceddad83e22fc026f
Calvin Roberts <roberts.calvin85@gmail.com>
2023-09-11 23:26:19 +0100

    Create function to style raindrops

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 'use strict';
 function rain() {
   let cloud = document.querySelector('.cloud'),
-    e = document.createElement('div');
+    e = document.createElement('div'),
+    left = Math.floor(Math.random() * 310),
+    width = Math.random() * 5,
+    height = Math.random() * 50,
+    duration = Math.random() * 0.5;
 
   e.classList.add('drop');
   cloud.appendChild(e);
+  e.style.left = left + 'px';
+  e.style.width = 0.5 + width + 'px';
+  e.style.height = 0.5 + height + 'px';
+  e.style.animationDuration = 1 + duration + 's';
 
   setTimeout(function () {
     cloud.removeChild(e);
